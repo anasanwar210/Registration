9423830d661ff3663dae8a09c5557d68afa568e2
Anas Anwar <anasanwar210@gmail.com>
2024-12-03 01:53:11 +0200

    api useing XML

--- js/welcome.js
+++ js/welcome.js
@@ -11,7 +11,10 @@ if (localStorage.getItem("currentUser")) {
 }
 
 let pathInWelcome = location.pathname;
-if (pathInWelcome.endsWith("/welcome.html") && localStorage.getItem("currentUser") === null) {
+if (
+  pathInWelcome.endsWith("/welcome.html") &&
+  localStorage.getItem("currentUser") === null
+) {
   location.assign("index.html");
   console.log("Stay in index");
 }
@@ -52,4 +55,39 @@ toggle.addEventListener("click", function (e) {
 function toggleMode() {
   document.body.classList.toggle("dark-mode");
   document.body.classList.toggle("light-mode");
+}
+
+/*
+==================================
+  - Get Data From Api
+==================================
+*/
+let cartona = ``;
+let myReq = new XMLHttpRequest();
+myReq.open("GET", "https://forkify-api.herokuapp.com/api/search?q=corn");
+myReq.onload = function () {
+  if (myReq.status === 200) {
+    let meals = JSON.parse(myReq.response);
+    displayMeals(meals);
+    console.log(meals.recipes);
+  } else {
+    console.log(`Error ${myReq.status}, ${myReq.statusText}`);
+  }
+};
+myReq.send();
+
+function displayMeals(meals) {
+  for (let i = 0; i < meals.recipes.length; i++) {
+    cartona += `
+        <div class="col-md-6 col-lg-4 mb-4 animate__animated animate__zoomIn" data-aos="fade-up" id="meal-card">
+      <div class="card">
+        <div class="card-img-overlay text-center d-flex flex-column justify-content-end overlay">
+          <h5 class="text-white meal-name">${meals.recipes[i].publisher}</h5>
+        </div>
+        <img src="${meals.recipes[i].image_url}" class="card-img-top" alt="Meal">
+      </div>
+    </div>
+  `;
+  }
+  document.getElementById("mealContainer").innerHTML = cartona;
 }
